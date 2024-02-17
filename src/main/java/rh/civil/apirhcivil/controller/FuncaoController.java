5e4a218948b975bbf47e0ebba9edb81ac95edca3
Mauro Silva <maurosilva@ifma.edu.br>
2024-02-16 21:34:29 -0300

    Método GET das Funções

--- src/main/java/rh/civil/apirhcivil/controller/FuncaoController.java
+++ src/main/java/rh/civil/apirhcivil/controller/FuncaoController.java
@@ -1,6 +1,7 @@
 package rh.civil.apirhcivil.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,8 @@ public class FuncaoController {
 		repository.save(new Funcao(dados));
 	}
 	
+	@GetMapping
+	@Transactional
 	public List<DadosListagemFuncao> listar() {
 		return repository.findAll().stream().map(DadosListagemFuncao::new).toList();
 	}
